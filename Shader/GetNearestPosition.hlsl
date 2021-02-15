2f585d37ce57af7c170a4067ee638c91d0393229
Umbrason <FabioKolligs@outlook.de>
2021-02-15 04:11:04 +0100

    Updated float3 to float4

--- Shader/GetNearestPosition.hlsl
+++ Shader/GetNearestPosition.hlsl
@@ -6,7 +6,7 @@ float SqrDst(float4 A, float3 Pos)
 }
 
 SamplerState my_point_clamp_sampler;
-void GetNearestPosition_float(float interactors, Texture2D positionTexture, float3 position, out float3 Out)
+void GetNearestPosition_float(float interactors, Texture2D positionTexture, float3 position, out float4 Out)
 {
     float4 sampled = positionTexture.SampleLevel(my_point_clamp_sampler,float2(.5 / interactors, 0),0);
     float4 closest = sampled;
