93d39dc554a77ecbe0b548078b703cb070330d91
Umbrason <FabioKolligs@outlook.de>
2021-02-15 23:13:20 +0100

    Update ShaderInteractor.cs

--- Scripts/ShaderInteractor.cs
+++ Scripts/ShaderInteractor.cs
@@ -30,7 +30,10 @@ public class ShaderInteractor : MonoBehaviour
         if (PositionDictionary.TryGetValue(material, out Dictionary<GameObject, Vector4> posDict))
         {
             Vector4[] positions = posDict.Values.ToArray();
-            Texture2D texture2D = oldTex?.width == positions.Length ? oldTex : new Texture2D(positions.Length, 1, TextureFormat.RGBAFloat, 0, true);
+            Texture2D texture2D = oldTex;
+            if (oldTex == null || oldTex.width != positions.Length)            
+                texture2D = new Texture2D(positions.Length, 1, TextureFormat.RGBAFloat, 0, true);
+            
 
             material.SetTexture("_InteractorPositions", texture2D);
             material.SetFloat("_Interactors", positions.Length);
