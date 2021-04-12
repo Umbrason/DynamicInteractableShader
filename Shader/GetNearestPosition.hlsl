b1e6fa5a23d2d9b711c4a2efc3cea8b59cbb3476
Umbrason <FabioKolligs@outlook.de>
2021-04-13 00:41:45 +0200

    Update GetNearestPosition.hlsl

--- Shader/GetNearestPosition.hlsl
+++ Shader/GetNearestPosition.hlsl
@@ -1,10 +1,8 @@
-
-
 float SqrDst(float4 A, float3 Pos)
 {
     return (A.x - Pos.x) * (A.x - Pos.x) + (A.y - Pos.y) * (A.y - Pos.y) + (A.z - Pos.z) * (A.z - Pos.z);
 }
-float Square(float A)
+float SquareClamp(float A)
 {
     return max(0.001f, A * A);
 }
@@ -16,11 +14,11 @@ void GetNearestPosition_float(float interactors, float channel, float channelCou
     float channelInteractionCount = positionTexture.SampleLevel(my_point_clamp_sampler, float2(.5f, channelIndex),0);
     float4 cur = positionTexture.SampleLevel(my_point_clamp_sampler, float2(1.5f / interactors, channelIndex),0);
     float4 closest = cur;
-    float closestSqrDst = SqrDst(closest, position) / Square(A);
+    float closestSqrDst = SqrDst(closest, position) / SquareClamp(A);
     for(int i = 1; i < channelInteractionCount; i++)
     {
         cur = positionTexture.SampleLevel(my_point_clamp_sampler, float2((i+1.5f) / interactors, channelIndex),0);
-        float sqrDst = SqrDst(cur, position) / Square(A);
+        float sqrDst = SqrDst(cur, position) / SquareClamp(A);
         if(closestSqrDst >= sqrDst) {
             closest = cur;
             closestSqrDst = sqrDst;
